6b4f265d5739d8ca9be555397a8b7c039d8b6e9d
TriptiGautam <gautamtripti1@gmail.com>
2024-05-02 15:03:11 +0545

    nav toggle started

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".menu");
+    menu.classList.toggle("show");
+    icon.classList.toggle("show");
+}
